6267bd5c094e545a805f80a9d52804a599712eba
miguel windows <miguel.atienza@physik.uni-muenchen.de>
2023-03-14 15:59:20 +0100

    no idea what happened here

---
+++ onedcellsim/compression/feature_net.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+
+class SummaryNet(nn.Module):
+    """A Summary net to convert a trajectory into a feature map. This 
+
+    Args:
+        nn (_type_): _description_
+    """
