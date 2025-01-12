86253cea113300f0285536408da690425ac0133e
Miguel Atienza <miguel.atienza@physik.uni-muenchen.de>
2025-01-12 17:56:40 +0100

    corrected absolute path for gui.sh

--- gui.sh
+++ gui.sh
@@ -4,4 +4,4 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PATH_TO_JULIA=$script_dir/venv/julia-1.6.7/bin/julia
 
 source $script_dir/venv/bin/activate
-python /home/miguel/onedcellsim_draft/onedcellsim/onedcellsim/simulators/multistability/viewer.py ${PATH_TO_JULIA}
+python $script_dir/onedcellsim/simulators/multistability/viewer.py ${PATH_TO_JULIA}
